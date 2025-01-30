4cfa427cdb072951b6f0c9f043a1f290fcaec279
Ange230700 <kouakouangeericstephane@gmail.com>
2025-01-30 10:42:43 +0100

    fix: 🐛  encapsulated calculator state

--- src/javascript/state/management.js
+++ src/javascript/state/management.js
@@ -1,34 +1,53 @@
 // src\javascript\state\management.js
 
-const globalVariables = {
-  first_number: "",
-  second_number: "",
-  current_operator: null,
-  has_result: false,
-};
+export const CalculatorState = (() => {
+  let first_number = "";
+  let second_number = "";
+  let current_operator = null;
+  let has_result = false;
 
-function resetGlobalVariables() {
-  globalVariables.first_number = "";
-  globalVariables.second_number = "";
-  globalVariables.current_operator = null;
-  globalVariables.has_result = false;
-}
+  function resetGlobalVariables() {
+    first_number = "";
+    second_number = "";
+    current_operator = null;
+    has_result = false;
+  }
 
-function saveResultForFurtherCalculation(result) {
-  globalVariables.first_number = result;
-  globalVariables.second_number = "";
-  globalVariables.current_operator = null;
-  globalVariables.has_result = true;
-}
+  function saveResultForFurtherCalculation(result) {
+    first_number = result;
+    second_number = "";
+    current_operator = null;
+    has_result = true;
+  }
 
-function chooseOperator(key) {
-  globalVariables.current_operator = key;
-  globalVariables.has_result = false;
-}
+  function chooseOperator(operatorKey) {
+    current_operator = operatorKey;
+    has_result = false;
+  }
 
-export {
-  globalVariables,
-  resetGlobalVariables,
-  saveResultForFurtherCalculation,
-  chooseOperator,
-};
+  function appendToFirstNumber(digit) {
+    first_number += digit;
+  }
+
+  function appendToSecondNumber(digit) {
+    second_number += digit;
+  }
+
+  function getState() {
+    return {
+      first_number,
+      second_number,
+      current_operator,
+      has_result,
+    };
+  }
+
+  return {
+    resetGlobalVariables,
+    saveResultForFurtherCalculation,
+    chooseOperator,
+    appendToFirstNumber,
+    appendToSecondNumber,
+    getState,
+  };
+})();
